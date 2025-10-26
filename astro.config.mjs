a4ad4199a88d67b75519a3b48b127bfb1b54b0c6
ascpixi <44982772+ascpixi@users.noreply.github.com>
2025-10-25 23:48:56 -0400

    feat: add security configuration to allow cross-origin requests

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import node from '@astrojs/node';
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  security: {
+    checkOrigin: false // we NEED to do a cross-origin request (Airtable -> iplace)
+  },
   adapter: node({
     mode: "standalone"
   })
